19116c667e3740a14aaa70e7b4c2bda25566fbb4
svaneven <svaneven@TC13-M900-22>
2019-04-25 17:46:46 -0500

    4/25/2019 - Full DB Deployment Added

---
+++ AdventureLogDB/Script.PreDeployment1.sql
@@ -0,0 +1,15 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+IF NOT EXISTS (SELECT * from sys.credentials WHERE name = 'AdventureLogUser')
+	CREATE CREDENTIAL [AdventureLogUser]
+		WITH IDENTITY = N'AdventureLogUser',
+		SECRET = N'Mc5jXq!tJJIZg1v9nozY'
